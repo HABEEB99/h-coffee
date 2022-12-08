9638cea46624b213fbe84804636f44334482fa25
Habeeb <habeebahmadu1@gmail.com>
2022-12-08 13:13:08 +0100

    Upgraded from Next.js version 12 to 13

---
+++ client/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>H-COFFEE</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/cup.png" />
+    </>
+  );
+}
